dce1b54345e32cf0961d804b64ae9a17536aa8f1
taka-kana <seedprofit@gmail.com>
2022-04-11 09:49:11 +0900

    use〜stotrage修正

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
