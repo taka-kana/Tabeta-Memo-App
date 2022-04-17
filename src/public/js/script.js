c6a9b8697401097ab7d99b172216cc466225e187
taka-kana <seedprofit@gmail.com>
2022-04-17 14:27:36 +0900

    UIを一部変更

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -70,3 +70,15 @@ $(function(){
       $('.flash_message').fadeOut(5000);
     });
   })();
+
+/*==================================================================
+# URL検出
+==================================================================*/
+  function AutoLink(str) {
+    var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
+    var regexp_makeLink = function(all, url, h, href) {
+	return '<a href="h' + href + '">' + url + '</a>';
+    }
+
+    return str.replace(regexp_url, regexp_makeLink);
+}
